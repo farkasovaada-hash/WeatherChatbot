86089cfb1b3ee0aba4b69a7cfc1340c99148b086
farkasovaada-hash <farkasova.ada@gmail.com>
2025-07-28 18:21:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-API_KEY = 'your_api_key_here'
+API_KEY = os.getenv("API_KEY")
 BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
 
 def get_weather(city):
